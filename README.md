094a59fb1f411e1907dff638b588ab8414b98e0e
Milenasilvadefreitas170 <milenasilvadefreitas170@gmail.com>
2021-11-10 22:05:48 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-4hx4iq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-4hx4iq)
---
+++ index.ts
@@ -0,0 +1,6 @@
+
+/* Questão 2 */
+const modulo1: number = parseInt(prompt('M1: '));
+const modulo2: number = parseInt(prompt('M2: '));
+ let media: number = (modulo1 * 4 + modulo2 * 6) / 10;
+ document.write('<p>Média: ' +media);
